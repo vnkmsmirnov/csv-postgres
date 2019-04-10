2eacb862af6eca04f0a6816ce807ae36ac4fc55c
Mikhail Smirnov <vnk.m.smirnov@gmail.com>
2019-04-10 17:28:36 +0300

    test

---
+++ csv-postgres/src/main/java/com/test/csvpostgres/CsvPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.test.csvpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvPostgresApplication.class, args);
+	}
+
+}
